db8d22caab528a9042e8eaa2d5c717b21aab399d
rahul-b-p <rahulzantoz10@gmail.com>
2024-11-18 18:10:14 +0530

    updated

---
+++ nodeEvents.ts
@@ -0,0 +1,14 @@
+import { EventEmitter } from 'node:events';
+
+class MyEmitter extends EventEmitter {
+  constructor() {
+    super();
+    this.emit('event');
+  }
+}
+
+const myEmitter = new MyEmitter();
+myEmitter.on('event', () => {
+  console.log('An event occurred!');
+});
+
